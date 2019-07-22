cef6c78f5a993e6147c9e168a67e6eeab70cdcef
tian <skynapier@outlook.com>
2019-07-23 00:29:23 +1200

    update mysql

--- server_mysql.js
+++ server_mysql.js
@@ -9,7 +9,7 @@ var connection = mysql.createConnection({
     host:'localhost',
     port:'3306',
     user:'root',
-    password:'Fate0889.',
+    password:'skynapier',
     database:'family_tree'
     });
 
